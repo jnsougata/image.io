768dac219132b1fb4a70b6c35cf8067d8039bd36
Zen <dwipjana293@gmail.com>
2021-09-06 01:00:54 +0530

    Update ImageIO.py

--- src/ImageIO.py
+++ src/ImageIO.py
@@ -10,15 +10,13 @@ class Figure:
 
 
     @staticmethod
-    def draw_figure(size: Tuple, color: hex = None):
+    def draw(size: Tuple, color: hex or str = None):
         color = 0x36393f if color is None else color
         new_image = Image.new("RGB", size, color=color)
         buff = io.BytesIO()
         new_image.save(buff, 'png')
         buff.seek(0)
         return buff
-
-
 
 
     @staticmethod
@@ -30,8 +28,7 @@ class Figure:
 
 class Canvas:
 
-    def __init__(self, size:Tuple, color:hex = None):
-
+    def __init__(self, size:Tuple, color:hex or str = None):
         color = 0x36393f if color is None else color
         size = size if size is not None and len(size) == 2 else None
         card = Image.new("RGB", size, color = color)
@@ -40,81 +37,88 @@ class Canvas:
         buff.seek(0)
         self.width = size[0]
         self.height = size[1]
-        self.canvas = buff
+        self.output = buff
 
 
     def show(self):
-        image = Image.open(self.canvas)
+        image = Image.open(self.output)
         image.show()
 
 
     def set_background(self, _byte = None, _path:str = None):
-
-        canvas = Image.open(self.canvas)
+        canvas = Image.open(self.output)
         size = canvas.size
-
         if _path is not None and _byte is None:
             bg = Image.open(_path)
         elif _byte is not None and _path is None:
             bg = Image.open(_byte)
         else:
-            bg = None
+            raise Exception("Use either _path or _byte")
+
 
         if bg is not None:
             _bg = bg.resize(size)
             buff = io.BytesIO()
             _bg.save(buff, 'png')
             buff.seek(0)
-            self.canvas = buff
+            self.output = buff
         else:
-            raise Exception("Use either _path or _byte")
+            raise TypeError("Image can not be NoneType")
 
 
 
     def add_image(self, _path:str = None, _byte = None, resize:Tuple = None, crop:Tuple = None, position:Tuple = None):
-
+        canvas = Image.open(self.output)
         if _path is not None and _byte is None:
             img = Image.open(_path)
         elif _byte is not None and _path is None:
             img = Image.open(_byte)
         else:
-            img = None
+            raise Exception("Use either _path or _byte")
+
 
         if img is not None:
+
             if resize is not None and crop is None:
-                if position is None:
-                    offset = ((self.width - resize[0]) // 2, (self.height - resize[1]) // 2)
-                else:
-                    offset = (position[0], position[1])
+                auto_align = ((self.width - resize[0]) // 2, (self.height - resize[1]) // 2)
+                manual_align = position
+                offset = auto_align if position is None else manual_align
                 _img = img.resize(resize, resample=0)
-                canvas = Image.open(self.canvas)
                 Image.Image.paste(canvas, _img, offset)
                 buff = io.BytesIO()
                 canvas.save(buff,'png')
                 buff.seek(0)
-                self.canvas = buff
-
+                self.output = buff
             elif crop is not None and resize is None:
                 _img = img.crop(crop)
                 dim = _img.size
-                if position is None:
-                    offset = ((self.width - dim[0]) // 2, (self.height - dim[1]) // 2)
-                else:
-                    offset = (position[0], position[1])
-                canvas = Image.open(self.canvas)
+                auto_align = ((self.width - dim[0]) // 2, (self.height - dim[1]) // 2)
+                manual_align = position
+                offset = auto_align if position is None else manual_align
                 Image.Image.paste(canvas, _img, offset)
                 buff = io.BytesIO()
                 canvas.save(buff, 'png')
                 buff.seek(0)
-                self.canvas = buff
+                self.output = buff
+            elif crop is None and resize is None:
+                size = img.size
+                auto_align = ((self.width - size[0]) // 2, (self.height - size[1]) // 2)
+                manual_align = (position[0], position[1])
+                offset = auto_align if position is None else manual_align
+                Image.Image.paste(canvas, img, offset)
+                buff = io.BytesIO()
+                canvas.save(buff, 'png')
+                buff.seek(0)
+                self.output = buff
             else:
-                raise Exception("Use either Crop or Resize")
+                raise Exception('Use either Resize or Crop')
         else:
-            raise TypeError('Image can\'t be NoneType')
+            raise TypeError('Image can not be NoneType')
             
 
 
-    def add_round_image(self, _path:str = None, _byte = None, resize:Tuple = None, position:Tuple = None):
+    def add_round_image(self, _path:str = None, _byte = None, resize:Tuple = None, crop:Tuple = None, position:Tuple = None):
+        canvas = Image.open(self.output)
         if _path is not None and _byte is None:
             img = Image.open(_path)
         elif _byte is not None and _path is None:
@@ -122,25 +126,53 @@ class Canvas:
         else:
             img = None
 
+
         if img is not None:
 
-            main = img if resize is None else img.resize(resize)
-            canvas = Image.open(self.canvas)
-
-            mask = Image.new("L", main.size, 0)
-            draw = ImageDraw.Draw(mask)
-            draw.pieslice([(0, 0), main.size], 0, 360,fill=255, outline="white")
-            dim = main.size
-            if position is None:
-                offset = ((self.width - dim[0]) // 2, (self.height - dim[1]) // 2)
+            if resize is not None and crop is None:
+                main = img.resize(resize)
+                mask = Image.new("L", main.size, 0)
+                draw = ImageDraw.Draw(mask)
+                draw.pieslice([(0, 0), main.size], 0, 360,fill=255, outline="white")
+                dim = main.size
+                auto_align = ((self.width - dim[0]) // 2, (self.height - dim[1]) // 2)
+                manual_align = position
+                offset = auto_align if position is None else manual_align
+                canvas.paste(main, offset, mask)
+                buff = io.BytesIO()
+                canvas.save(buff, 'png')
+                buff.seek(0)
+                self.output = buff
+            elif crop is not None and resize is None:
+                main = img.crop(crop)
+                mask = Image.new("L", main.size, 0)
+                draw = ImageDraw.Draw(mask)
+                draw.pieslice([(0, 0), main.size], 0, 360, fill=255, outline="white")
+                dim = main.size
+                auto_align = ((self.width - dim[0]) // 2, (self.height - dim[1]) // 2)
+                manual_align = position
+                offset = auto_align if position is None else manual_align
+                canvas.paste(main, offset, mask)
+                buff = io.BytesIO()
+                canvas.save(buff, 'png')
+                buff.seek(0)
+                self.output = buff
+            elif crop is None and resize is None:
+                main = img
+                mask = Image.new("L", main.size, 0)
+                draw = ImageDraw.Draw(mask)
+                draw.pieslice([(0, 0), main.size], 0, 360, fill=255, outline="white")
+                dim = main.size
+                auto_align = ((self.width - dim[0]) // 2, (self.height - dim[1]) // 2)
+                manual_align = position
+                offset = auto_align if position is None else manual_align
+                canvas.paste(main, offset, mask)
+                buff = io.BytesIO()
+                canvas.save(buff, 'png')
+                buff.seek(0)
+                self.output = buff
             else:
-                offset = (position[0], position[1])
-            canvas.paste(main, offset, mask)
-            buff = io.BytesIO()
-            canvas.save(buff, 'png')
-            buff.seek(0)
-            self.canvas = buff
-        else:
-            raise TypeError('Image can\'t be NoneType')
+                raise Exception('Use either Resize or Crop')
 
-            
+        else:
+            raise TypeError('Image can not be NoneType')
